771eaafced65965d0cc6fbb547a0b4b7e257742e
Uroosasaad <115679429+Uroosasaad@users.noreply.github.com>
2023-04-13 15:55:41 -0500

    Update HelloWorldApp.java
    cleaning up code!

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,20 +1,7 @@
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 class HelloWorldApp {
 
 	public static void main(String[] args) {
-		/*BufferedReader reader = new BufferedReader(
-			new InputStreamReader(System.in));
-		String name;
-		try{
-			System.out.println("Enter your name");
-			name = reader.readLine();
-			System.out.println("My Name is = " + name);
-		}
-		catch (Exception e) {
-
-		}*/
+	 
 		showMessage("Uroosa");
 	}
 	public static void showMessage(String name) {
