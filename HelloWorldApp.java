5a0fff0cf4756f89bf996d884a65d3cddf548ab9
Uroosasaad <115679429+Uroosasaad@users.noreply.github.com>
2023-04-15 01:28:16 -0500

    Update HelloWorldApp.java
    Simple System.outApp

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -2,9 +2,6 @@ class HelloWorldApp {
 
 	public static void main(String[] args) {
 	 
-		showMessage("Uroosa");
-	}
-	public static void showMessage(String name) {
-		System.out.println(name);
+		System.out.println("HelloWorld!");
 	}
 }
